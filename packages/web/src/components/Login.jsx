3c25302c68d523167cf041be4a1971fa36fca9c6
7lcclint <pakorn.clint@outlook.com>
2023-09-09 03:07:01 +0700

    commit api

--- packages/web/src/components/Login.jsx
+++ packages/web/src/components/Login.jsx
@@ -26,7 +26,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/logedIn', {
+      const response = await fetch('https://garageservice-api.vercel.app/logedIn', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
